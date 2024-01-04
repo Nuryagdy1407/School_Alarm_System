91c1be33a4537413d4d2cbb8c1a46151857441b7
Nuryagdy Azadov <74148048+Nuryagdy1407@users.noreply.github.com>
2024-01-04 11:12:47 +0500

    The main project

---
+++ School_Alarm_System/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace School_Alarm_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.lbl_wagt = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl_wagt
+            // 
+            this.lbl_wagt.AutoSize = true;
+            this.lbl_wagt.Location = new System.Drawing.Point(64, 160);
+            this.lbl_wagt.Name = "lbl_wagt";
+            this.lbl_wagt.Size = new System.Drawing.Size(35, 13);
+            this.lbl_wagt.TabIndex = 0;
+            this.lbl_wagt.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(934, 661);
+            this.Controls.Add(this.lbl_wagt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "School Alarm System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label lbl_wagt;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ School_Alarm_System/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_Alarm_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void PortBarlaweDak()
+        {
+            serialPort1.Close();
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine(ports);
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                serialPort1.PortName = port;
+            }
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Maglumat beriji enjamlar bilen baglanyşyk kesildi! Baglanyşygy barlaň!");
+                Form1_Load(MessageBoxButtons.OKCancel, null);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PortBarlaweDak();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //autoRefresh++;
+            string s, m, se;
+
+            if (Convert.ToInt32(DateTime.Now.Hour) < 10) { s = "0" + DateTime.Now.Hour.ToString(); }
+            else { s = DateTime.Now.Hour.ToString(); }
+
+            if (Convert.ToInt32(DateTime.Now.Minute) < 10) { m = "0" + DateTime.Now.Minute.ToString(); }
+            else { m = DateTime.Now.Minute.ToString(); }
+
+            if (Convert.ToInt32(DateTime.Now.Second) < 10) { se = "0" + DateTime.Now.Second.ToString(); }
+            else { se = DateTime.Now.Second.ToString(); }
+
+            lbl_wagt.Text = s + ":" + m + ":" + se;
+
+            if (lbl_wagt.Text == "08:30:00" || lbl_wagt.Text == "09:15:00" || lbl_wagt.Text == "09:20:00" || lbl_wagt.Text == "10:05:00" || lbl_wagt.Text == "10:10:00" || lbl_wagt.Text == "10:55:00" || lbl_wagt.Text == "11:25:00" || lbl_wagt.Text == "12:10:00" || lbl_wagt.Text == "12:15:00" || lbl_wagt.Text == "13:00:00" || lbl_wagt.Text == "13:05:00" || lbl_wagt.Text == "13:50:00" || lbl_wagt.Text == "14:30:00" || lbl_wagt.Text == "15:15:00") 
+            {
+                Saz_chal();
+            }
+        }
+
+        private void Saz_chal()
+        {
+            serialPort1.WriteLine("h");
+        }
+    }
+}
